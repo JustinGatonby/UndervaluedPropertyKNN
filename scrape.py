58e6f3b4e4fa83446f38cd041cdd12c2aa8ad481
JustinGatonby <justinagatonby@gmail.com>
2024-11-14 07:21:17 +0200

    Scaffolding scrape.py

--- scrape.py
+++ scrape.py
@@ -1,1 +1,28 @@
+from instream import InStream 
+import pandas as pd 
 
+def get_prices():
+    pass
+
+def get_type():
+    pass 
+
+def get_bedrooms():
+    pass
+
+def get_bathrooms():
+    pass
+
+def get_garage():
+    pass 
+
+def get_erf():
+    pass 
+
+def loop():
+    pass 
+
+def main():
+    pass 
+
+if __name__ == "__main__": main() 
